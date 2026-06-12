edaebd524f76cea2d6144b447f628bd15b559698
jiatang1603-byte <jiatang1603@gmail.com>
2026-06-12 15:20:40 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/inegrateQR/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
